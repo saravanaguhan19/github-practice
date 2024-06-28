d492e34cd124422a8bd6920e820882c7ac4fec24
saravanaguhan19 <saravanaguhan19@gmail.com>
2024-06-28 19:46:30 +0530

    6th commit

---
+++ const.js
@@ -0,0 +1,15 @@
+let value = "string";
+
+function countLetter(text) {
+  let count = 0;
+
+  for (let i = 0; i < text.length; i++) {
+    if (text.length == null) {
+      return count;
+    }
+    count = count + 1;
+  }
+  return count;
+}
+
+console.log(countLetter(" "));
